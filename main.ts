fefc0b5cafad3c6a50a13f346b2fb09c2c6ca5df
JA1934 <92798990+JA1934@users.noreply.github.com>
2021-11-10 09:17:35 -0500

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,6 +1,7 @@
 let degrees = 0
 basic.forever(function () {
     degrees = input.compassHeading()
+    // If you turn the micro bit a certain number of degrees it shows these messages.
     if (degrees < 45) {
         basic.showString("N")
     } else if (degrees < 135) {
